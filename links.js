59de485d20e6640730f94c6391b875dad034dd95
Ferit <69053849+Ruzgra@users.noreply.github.com>
2024-06-28 01:19:31 +0300

    Add files via upload

---
+++ links.js
@@ -0,0 +1,10 @@
+const gh = document.getElementById('gh')
+const yt = document.getElementById('yt')
+
+gh.addEventListener("click", () => {
+    window.open("https://github.com/Ruzgra", "_blank")
+})
+
+yt.addEventListener("click", () => {
+    window.open("https://www.youtube.com/", "_blank")
+})
